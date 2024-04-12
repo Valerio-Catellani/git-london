94e1c2a2b7faaf833c845c68490ebffdbffa5f4f
Valerio-Catellani <valeriocatellani@gmail.com>
2024-04-12 17:19:34 +0200

    valerio commit

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,17 @@
 <template>
     <header>
-        <h1>Il mio Viaggio a Londra</h1>
-        <img src="/images/pexels-bill-emrich-230794-0 (1).jpg" alt="london eye with London city skyline in the background" ><img src="/images/pexels-bill-emrich-230794-1.jpg" alt="London eye with london city skyline in the background" ><img src="/images/pexels-bill-emrich-230794-2.jpg" alt="london eye with london city skyline in the background" >
-        <hr>
+        <h1>Il tuo Viaggio a Londra</h1>
+        <img src="/images/pexels-bill-emrich-230794-0 (1).jpg"
+            alt="london eye with London city skyline in the background"><img
+            src="/images/pexels-bill-emrich-230794-1.jpg" alt="London eye with london city skyline in the background">
+        <div>ciao</div>
     </header>
 </template>
 
 <script>
-    export default {
-        name: 'HeaderComponent',
-    }
+export default {
+    name: 'HeaderComponent',
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
