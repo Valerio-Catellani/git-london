fe1ead9bb488d38ddade920ef58cf7afeb865959
Valerio-Catellani <valeriocatellani@gmail.com>
2024-04-12 16:39:04 +0200

    Info utili

---
+++ src/components/InfoComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <section>
+        <h3>Info Utili:</h3>
+        <ol>
+            <li>
+                <span>Numeri di Emergenza</span>
+                <ul>
+                    <li><b>999</b> - Emergenza</li>
+                    <li><b>101</b> - Numero polizia</li>
+                    <li><b>111</b> - Numero Assistenza Medica</li>
+                </ul>
+            </li>
+            <li>
+                <span>Hotel</span>
+                <ul>
+                    <li><b>Nome hotel</b>: Buckingham Palace</li>
+                    <li><b>Indirizzo hotel</b>: Westminster, London SW1A 1AA, Regno Unito</li>
+                </ul>
+            </li>
+            <li>
+                <span>Aereo</span>
+                <ul>
+                    <li><b>Volo di Andata</b>: YI234X, ore 9.30 da Milano Malpensa</li>
+                    <li><b>Volo di Ritorno</b>: YI236X, ore 18:30 da London Heathrow</li>
+                </ul>
+                <!-- insert comment line to suppress white space -->
+
+            </li>
+        </ol>
+    </section>
+    <hr />
+
+</template>
+
+<script>
+export default {
+    name: 'InfoComponent'
+}
+</script>
+
+<style lang="scss" scoped></style>
