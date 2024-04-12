c7daa5b61a1d900ec4157b04e1441e94da684c7b
danielbosa <danielbosa95@gmail.com>
2024-04-12 16:15:46 +0200

    ExpenseComponent

---
+++ src/components/ExpenseComponent.vue
@@ -0,0 +1,54 @@
+<template>
+        <section>
+            <h2>Le mie spese</h2>
+            <table>
+                <thead>        
+                    <tr>
+                        <th><strong>Numero</strong></th>
+                        <th><strong>Descrizione</strong></th>
+                        <th><strong>Costo</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Biglietto Aereo</td>
+                        <td>120€</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Biglietto Aereo</td>
+                        <td>120€</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Biglietto Aereo</td>
+                        <td>120€</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>Totale</td>
+                        <td>&nbsp;</td>
+                        <td>360€</td>
+                    </tr>
+                    <!-- altro modo per fare tabella, unendo le celle di colonna 1 e 2
+                    <tr>
+                        <td coldspan="2">Totale</td>
+                        <td>360€</td>
+                    </tr>-->
+                </tfoot>
+            </table>
+            <hr>
+        </section>
+</template>
+
+<script>
+    export default {
+        name: 'ExpenseComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
