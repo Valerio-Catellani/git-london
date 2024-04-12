531ff154d5e08cf007cd8953de028f147bf8c8b6
Gabrielesuma <gabrielesuma139@gmail.com>
2024-04-12 16:28:58 +0200

    cose da fare

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <HeaderComponent />
   <main>
     <ExpenseComponent/>
+    <ListToDoComponent/>
   </main>
 </template>
 
 <script>
 import ExpenseComponent from './components/ExpenseComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
+import ListToDoComponent from './components/ListToDoComponent.vue';
   export default {
     name: 'App',
     components: {
       HeaderComponent,
       ExpenseComponent,
+      ListToDoComponent
     }
   }
 </script>
